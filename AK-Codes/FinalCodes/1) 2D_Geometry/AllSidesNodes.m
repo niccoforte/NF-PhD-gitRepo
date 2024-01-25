724221b33e22d736ed8f77d0ea33064540dd61d1
Niccolò Forte <99287732+niccoforte@users.noreply.github.com>
2024-01-25 11:45:03 +0000

    Rearrange repo for NN.

--- AK-Codes/FinalCodes/1) 2D_Geometry/AllSidesNodes.m
+++
@@ -1,9 +0,0 @@
-function [topNodes, bottomNodes, leftNodes, rightNodes] = AllSidesNodes(nodes,L,H)
-% Find boundary nodes
-xCoord = nodes(:,2);
-yCoord = nodes(:,3);
-bottomNodes = find(yCoord>=-1e-3 & yCoord<=+1e-3);
-topNodes    = find(yCoord>=H-1e-3 & yCoord<=H+1e-3);
-leftNodes   = find(xCoord>=-1e-3 & xCoord<=+1e-3);
-rightNodes  = find(xCoord>=L-1e-3 & xCoord<=L+1e-3);
-end
