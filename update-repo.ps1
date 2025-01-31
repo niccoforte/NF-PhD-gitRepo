9e5d3d79354cc0ca4ea36903f39977906edf94b0
Niccolò Forte <exy053@qmul.ac.uk>
2025-01-31 01:00:15 +0000

    Auto-commit.

---
+++ update-repo.ps1
@@ -0,0 +1,13 @@
+cd C:\Users\exy053\Documents\p1git-Lattices
+git pull
+git add .
+git commit -m "Auto-commit."
+git push
+
+C:\Users\exy053\OneDrive - Queen Mary, University of London\Documents\Research\p1-LatticeFractureToughness\p1git-Lattices
+git pull
+git add .
+git commit -m "Auto-commit."
+git push
+
+cd C:\Users\exy053\Documents\p1git-Lattices
