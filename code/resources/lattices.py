c2c99ee99cf6084b40d1f9067e3e86b9090bfa7a
Niccolò Forte <exy053@qmul.ac.uk>
2025-03-07 10:26:15 +0000

    (OneDrive) Auto-commit.

--- code/resources/lattices.py
+++ code/resources/lattices.py
@@ -258,7 +258,7 @@ def effProperties(LAT, E_s, rD):
     return E, v
 
 
-def get_nodes(LAT, geom, dis, mode='lattice', stiff=False, path="C:/Users/exy053/Documents/", sim=1):
+def get_nodes(LAT, geom, dis, mode='lattice', stiff=False, path="C:/Users/exy053/Documents/", sim=1):  # TODO: Rename
     if mode.lower() == "unit":
         if LAT.lower() == "fcc":
             nodes = np.array([[0,0,0],
