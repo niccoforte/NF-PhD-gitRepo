e9baf026805da58f334a8e769ffbd46a6b603b5d
Niccolo Forte <exy053@qmul.ac.uk>
2025-03-10 11:58:14 +0000

    Repalce .ipynb ML data and functions files with .py imports.

---
+++ code/resources/MLdata.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pandas as pd
+from torch.utils.data.dataset import Dataset
+import sklearn
+
+### General Data Initialization
+
+
+def load_TrainTestData(CSV_train_in, CSV_train_out, CSV_val_in, CSV_val_out, CSV_test_in, CSV_test_out):
+    train_in = pd.read_csv(CSV_train_in, index_col=0, header=0).to_numpy()
+    train_out = pd.read_csv(CSV_train_out, index_col=0, header=0).to_numpy()
+    val_in = pd.read_csv(CSV_val_in, index_col=0, header=0).to_numpy()
+    val_out = pd.read_csv(CSV_val_out, index_col=0, header=0).to_numpy()
+    test_in = pd.read_csv(CSV_test_in, index_col=0, header=0).to_numpy()
+    test_out = pd.read_csv(CSV_test_out, index_col=0, header=0).to_numpy()
+    return train_in, train_out, val_in, val_out, test_in, test_out
+
+def load_perData(INcsv, OUTcsv):
+    IN_df = pd.read_csv(INcsv, index_col=0).sort_index()
+    OUT_df = pd.read_csv(OUTcsv, index_col=0).sort_index()
+    perIN_df = IN_df.loc[:0]
+    perOUT_df = OUT_df.loc[:0]
+    return perIN_df.to_numpy()[0], perOUT_df.to_numpy()[:,1:]
+
+
+def dataParams(x):
+    return [np.min(x), np.max(x), np.mean(x), np.std(x)]
+
+def standardize(x, minx, maxx, mode=0):
+    if mode == 0:
+        return (x - minx)/(maxx - minx)
+    if mode == 1:
+        return (x*(maxx - minx)) + minx
+    
+def normalize(x, mean, std, mode=0):
+    if mode == 0:
+        return (x - mean)/std
+    if mode == 1:
+        return (x*std) + mean
+
+class Dataset_(Dataset):
+    def __init__(self, x, y, edge_index=None):
+        self.x = x
+        self.y = y
+        self.edge_index=edge_index
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+    def __len__(self):
+        return self.x.shape[0]
+    
+
+class DATA:
+    def __init__(self, path=0, load=False, LAT="FCC", dis="dN", dN=20, DisDist=0):
+        self.path = path
+        self.LAT = LAT
+        self.dis = dis
+        self.dN = dN
+
+        self.get_DataPath()
+
+        if load:
+            self.get_DataFiles()
+            self.load_data()
+
+            if DisDist == 1:
+                self.load_DisDist_v1()
+            elif DisDist == 2:
+                self.load_DisDist_v2()
+
+
+    def get_DataPath(self):
+        pData = 'data/'
+
+        pAl          = pData + 'Al/'
+        pAK          = pAl + 'AK/'
+        pUTdisNodes  = pAK + 'Ductile-disNodes-FCC-12X16/'
+        pUTdisNodes2 = pAK + '20_RD02_10mm/'
+        pUTdisStruts = pAK + 'Ductile-disStruts-FCC-12X16/'
+        pFTdisNodes  = pAK + 'Fracture-disNodes/'
+
+        pTi    = pData + 'Ti/'
+        pTiLAT = pTi + f'{self.dN}dN/{self.LAT}/'
+
+        if self.path == 0:
+            self.PATH  = pUTdisNodes2
+        elif self.path == 1:
+            self.PATH  = pTiLAT
+
+    def get_DataFiles(self):
+
+        self.CSV_train_in  = self.PATH + f'NN-UT-{self.dis}-trainIN.csv'
+        self.CSV_train_out = self.PATH + f'NN-UT-{self.dis}-trainOUT.csv'
+        self.CSV_val_in  = self.PATH + f'NN-UT-{self.dis}-valIN.csv'
+        self.CSV_val_out = self.PATH + f'NN-UT-{self.dis}-valOUT.csv'
+        self.CSV_test_in  = self.PATH + f'NN-UT-{self.dis}-testIN.csv'
+        self.CSV_test_out = self.PATH + f'NN-UT-{self.dis}-testOUT.csv'
+
+        self.INcsv = self.PATH + f'Ductile-disNodes-IN.csv'
+        self.OUTcsv = self.PATH + f'Ductile-disNodes-OUT.csv'
+
+    def load_data(self):
+        self.train_in, self.train_out, self.val_in, self.val_out, self.test_in, self.test_out = load_TrainTestData(self.CSV_train_in, 
+                                                                                                                   self.CSV_train_out, 
+                                                                                                                   self.CSV_val_in, 
+                                                                                                                   self.CSV_val_out, 
+                                                                                                                   self.CSV_test_in, 
+                                                                                                                   self.CSV_test_out)
+        self.perIN, self.perOUT = load_perData(self.INcsv, self.OUTcsv)
+
+        self.inParams = dataParams(np.concatenate((self.train_in, self.val_in, self.test_in)))
+        self.outParams = dataParams(np.concatenate((self.train_out, self.val_out, self.test_out)))
+
+        self.train_inST = standardize(self.train_in, self.inParams[0], self.inParams[1])
+        self.train_outST = standardize(self.train_out, self.outParams[0], self.outParams[1])
+        self.val_inST = standardize(self.val_in, self.inParams[0], self.inParams[1])
+        self.val_outST = standardize(self.val_out, self.outParams[0], self.outParams[1])
+        self.test_inST = standardize(self.test_in, self.inParams[0], self.inParams[1])
+        self.test_outST = standardize(self.test_out, self.outParams[0], self.outParams[1])
+        
+        self.train_inNM = normalize(self.train_in, self.inParams[2], self.inParams[3])
+        self.train_outNM = normalize(self.train_out, self.outParams[2], self.outParams[3])
+        self.val_inNM = normalize(self.val_in, self.inParams[2], self.inParams[3])
+        self.val_outNM = normalize(self.val_out, self.outParams[2], self.outParams[3])
+        self.test_inNM = normalize(self.test_in, self.inParams[2], self.inParams[3])
+        self.test_outNM = normalize(self.test_out, self.outParams[2], self.outParams[3])
+    
+    def load_DisDist_v1(self):
+        train_in1 = self.perIN.reshape(int(len(self.perIN)/2), 2)
+        self.train_in1 = np.array([i for i in train_in1 if max(train_in1[:,0]) != i[0] and min(train_in1[:,0]) != i[0] and 
+                                                           max(train_in1[:,1]) != i[1] and min(train_in1[:,1]) != i[1]])
+
+        train_out1 = self.train_in.reshape(len(self.train_in),int(len(self.train_in[0])/2),2)
+        self.dx_out1 = train_out1[:,:,0].reshape(len(self.train_in),int(len(self.train_in[0])/2),1)
+        self.dy_out1 = train_out1[:,:,1].reshape(len(self.train_in),int(len(self.train_in[0])/2),1)
+
+        self.inParams1 = dataParams(self.train_in1)
+        self.train_in1ST = standardize(self.train_in1, self.inParams1[0], self.inParams1[1])
+        self.train_in1NM = normalize(self.train_in1, self.inParams1[2], self.inParams1[3])
+
+        self.outParams1dx = dataParams(self.dx_out1)
+        self.outParams1dy = dataParams(self.dy_out1)
+        self.dx_out1ST = standardize(self.dx_out1, self.outParams1dx[0], self.outParams1dx[1])
+        self.dy_out1ST = standardize(self.dy_out1, self.outParams1dy[0], self.outParams1dy[1])
+        self.dx_out1NM = normalize(self.dx_out1, self.outParams1dx[2], self.outParams1dx[3])
+        self.dy_out1NM = normalize(self.dy_out1, self.outParams1dy[2], self.outParams1dy[3])
+    
+    def load_DisDist_v2(self):
+        train_in1 = self.perIN.reshape(int(len(self.perIN)/2), 2)
+        self.train_in1 = np.array([i for i in train_in1 if max(train_in1[:,0]) != i[0] and min(train_in1[:,0]) != i[0] and 
+                                                           max(train_in1[:,1]) != i[1] and min(train_in1[:,1]) != i[1]])
+        self.train_in2 = np.array([self.train_in1.flatten()]*2)
+
+        train_out2 = self.train_in.reshape(len(self.train_in),int(len(self.train_in[0])/2),2)
+        dx_out2 = train_out2[:,:,0].reshape(len(self.train_in),int(len(self.train_in[0])/2))
+        self.dx_out2 = np.stack((dx_out2, dx_out2), axis=1)
+        dy_out2 = train_out2[:,:,1].reshape(len(self.train_in),int(len(self.train_in[0])/2))
+        self.dy_out2 = np.stack((dy_out2, dy_out2), axis=1)
+
+        self.inParams2 = dataParams(self.train_in2)
+        self.train_in2ST = standardize(self.train_in2, self.inParams2[0], self.inParams2[1])
+        self.train_in2NM = normalize(self.train_in2, self.inParams2[2], self.inParams2[3])
+
+        self.outParams2dx = dataParams(self.dx_out2)
+        self.outParams2dy = dataParams(self.dx_out2)
+        self.dx_out2ST = standardize(self.dx_out2, self.outParams2dx[0], self.outParams2dx[1])
+        self.dx_out2ST = standardize(self.dx_out2, self.outParams2dy[0], self.outParams2dy[1])
+        self.dx_out2NM = normalize(self.dx_out2, self.outParams2dx[2], self.outParams2dx[3])
+        self.dx_out2NM = normalize(self.dx_out2, self.outParams2dy[2], self.outParams2dy[3])
+
---
+++ code/resources/MLfunc.py
@@ -0,0 +1,223 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+from torch_geometric.utils import to_networkx
+import networkx as nx
+
+
+### Machine Learning Functions
+
+def train_model(typ, model, lossf, n_epochs, opt, train_dataloader, val_dataloader=None, scheduler=None, earlyStop=None, verbose=10):
+    train_lossLog = []
+    val_lossLog = []
+    best_loss, best_epoch = 1000, 0
+    for epoch in range(1, n_epochs+1):
+        train_lossSum = 0
+        for batch in train_dataloader:
+            if typ.lower() == "gcn":
+                x, y = batch.x.float(), batch.y.float()
+                y_predict = model(batch.x, batch.edge_index, batch.batch)
+                y = batch.y.view(batch.num_graphs, -1)
+            else:
+                x, y = batch[:][0].float(), batch[:][1].float()
+                y_predict = model(x)
+            loss = lossf(y_predict, y)
+            train_lossSum += loss
+
+            opt.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+            opt.step()
+
+            train_lossLog.append(loss.item())
+        train_lossAvg = train_lossSum/len(train_dataloader)
+        
+        if val_dataloader:
+            with torch.no_grad():
+                val_lossSum = 0
+                for batch in val_dataloader:
+                    if typ.lower() == "gcn":
+                        x, y = batch.x.float(), batch.y.float()
+                        y_predict = model(batch.x, batch.edge_index, batch.batch)
+                        y = batch.y.view(batch.num_graphs, -1)
+                    else:
+                        x, y = batch[:][0].float(), batch[:][1].float()
+                        y_predict = model(x)
+                    loss = lossf(y_predict, y)
+
+                    val_lossLog.append(loss.item())
+                    val_lossSum += loss
+
+                val_lossAvg = val_lossSum/len(val_dataloader)
+            if scheduler:
+                scheduler.step(val_lossAvg)
+            if earlyStop:
+                earlyStop(val_lossAvg)
+                if earlyStop.early_stop:
+                    break
+        else:
+            if scheduler:
+                scheduler.step(train_lossAvg)
+            if earlyStop:
+                earlyStop(train_lossAvg)
+                if earlyStop.early_stop:
+                    break
+        
+        if torch.abs(loss) < best_loss:
+            best_loss = torch.abs(loss).item()
+            best_epoch = epoch
+        
+        if verbose:
+            if epoch == 1 or epoch % int(verbose) == 0:
+                print("Epoch:", epoch, "- Loss:", loss.item())
+            
+    print(f"Best Epoch: {best_epoch}, with loss {best_loss}")
+    # torch.save(model.state_dict(), "new_best_deep_ritz1.mdl")
+    return model, epoch, train_lossLog, val_lossLog
+
+def predict_model(typ, model, test_dataloader):
+    test_outputs = []
+    with torch.no_grad():
+        for batch in test_dataloader:
+            if typ.lower() == "gcn":
+                x, y = batch.x.float(), batch.y.float()
+                y_predict = model(batch.x, batch.edge_index, batch.batch)
+                y = batch.y.view(batch.num_graphs, -1)
+            else:
+                x, y = batch[:][0].float(), batch[:][1].float()
+                y_predict = model(x)
+            test_outputs.append(y_predict.detach().cpu().numpy())
+    test_outputs = np.concatenate(test_outputs)
+    return test_outputs
+
+def weights_init(m, dist="xavier"):
+    if isinstance(m, nn.Linear):
+        if dist.lower() == "xavier":
+            nn.init.xavier_normal_(m.weight)
+        elif dist.lower() == "kaiming":
+            nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+        nn.init.constant_(m.bias, 0.0)
+
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0.0001, verbose=True):
+        self.patience = patience
+        self.min_delta = min_delta
+        self.verbose = verbose
+        self.best_loss = float('inf')
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if val_loss < self.best_loss - self.min_delta:
+            self.best_loss = val_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+        
+        if self.counter >= self.patience:
+            if self.verbose:
+                print(f"Early stopping triggered after {self.patience} epochs without improvement.")
+            self.early_stop = True  # Set flag for stopping
+
+
+### Custom Loss Functions
+
+def QuantileLoss():
+    pass
+
+def custom_loss(target, output):   ### TODO: Quantile loss
+    return torch.mean((output - target)**2)
+
+def physics_loss(target, output):
+    return torch.abs(torch.sum(target) - torch.sum(output))
+
+
+### Plotting Functions
+
+def plot_loss(epoch, train, val):
+    fig = plt.figure(figsize=(10, 5))
+    plt.plot(np.linspace(1, epoch, len(train)), train)
+    plt.plot(np.linspace(1, epoch, len(val)), val)
+    plt.xlabel("Training Epoch")
+    plt.ylabel("Mean Squared Error (MSE)")
+    plt.yscale("log")
+    plt.title("Training and Validation Loss Vs Epoch")
+    plt.legend(["Training", "Validation"])
+    plt.grid()
+    plt.show()
+
+def plot_StressStrainOUT(perOUT, train_out, test_outputs):
+    fig = plt.figure(figsize=(10, 5))
+    plt.scatter(perOUT[0], train_out[10]+perOUT[1], s=5, label="Truth")
+    plt.scatter(perOUT[0], test_outputs[10]+perOUT[1], s=5, label="Prediction")
+    plt.ylabel("Stress ($\sigma$) [MPa]")
+    plt.xlabel("Strain ($\epsilon$)")
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+# TODO: ERROR PLOTTING 
+
+def plot_Distribution(train_in1, test_outputs, dx_out1=None, typ="contour"):
+    x_, y_ = train_in1[:,0], train_in1[:,1]
+    if dx_out1 is not None:
+        val_ = dx_out1 - test_outputs
+        title = "Error Distribution"
+    else:
+        val_ = test_outputs
+        title = "Output Distribution"
+
+    if typ.lower() == "contour" or typ.lower() == "both":
+        fig1 = plt.figure(figsize=(10, 6))
+        ax = fig1.add_subplot()
+        ax.set_title(title)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ctr = ax.tricontourf(x_, y_, val_, cmap="viridis")
+        plt.colorbar(ctr)
+    if typ.lower() == "3d" or typ.lower() == "both":
+        fig2 = plt.figure(figsize=(10, 6))
+        ax = fig2.add_subplot(111, projection='3d')
+        ax.set_title(title)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ctr = ax.plot_trisurf(x_, y_, val_, cmap="viridis")
+        plt.colorbar(ctr) 
+    plt.show()
+
+
+### GNN Functions
+
+def visualize_graphNetwork(loader, colors=None, layout="kk"):
+    for batch in loader:
+        dat = batch.get_example(0)
+        break
+    G = to_networkx(dat, to_undirected=True)
+    
+    plt.figure(figsize=(7,7))
+    if layout.lower() == "kk":
+        pos = nx.kamada_kawai_layout(G)
+    elif layout.lower() == "spec":
+        pos = nx.spectral_layout(G)
+    elif layout.lower() == "spring":
+        pos = nx.spring_layout(G, seed=1)
+    elif layout.lower() == "planar":
+        pos = nx.planar_layout(G)
+    elif layout.lower() == "rand":
+        pos = nx.random_layout(G)
+    
+    if colors is None:
+        colors = dat.x[:, 0].detach().numpy()
+    
+    nx.draw_networkx_nodes(G, pos, node_size=20, node_color=colors, cmap='viridis')
+    nx.draw_networkx_edges(G, pos, alpha=0.5, width=0.5)
+    
+    plt.show()
+
