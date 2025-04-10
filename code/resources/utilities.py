39c67a30213c0d41c5b2b66ea4f90059fe2e06b8
Niccolo Forte <exy053@qmul.ac.uk>
2025-04-10 16:52:06 +0100

    Update to convention.

--- code/resources/utilities.py
+++ code/resources/utilities.py
@@ -8,8 +8,6 @@ def addPredix():
         if file.name.endswith('.txt') and 'IN-' not in file.name and 'OUT-' not in file.name:
             #print(path + file.name)
             os.rename(path+file.name, f'{path}OUT-{file.name}')
-        else:
-            pass
 
 def TXTtoCSV():
     ## Change .txt file to .csv file.
@@ -18,8 +16,6 @@ def TXTtoCSV():
         if file.name.endswith('.txt') and 'IN-' not in file.name and 'OUT-' not in file.name:
             #print(path + file.name)
             os.rename(path+file.name, f'{path}OUT-{file.name}')
-        else:
-            pass
 
 def renumber():
     ## Change simulation numbering
@@ -28,5 +24,3 @@ def renumber():
         if file.name.endswith('.csv') and 'IN-' not in file.name and 'OUT-' not in file.name:
             #print(path + file.name)
             os.rename(path+file.name, f'{path}OUT-{file.name}')
-        else:
-            passs
