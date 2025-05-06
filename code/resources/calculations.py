ff5f572b1214c80038e23536e3110501b930a1a4
Niccolo Forte <exy053@qmul.ac.uk>
2025-05-06 18:48:18 +0100

    Add delimited parameter to get_ductileData function for Akash data.

--- code/resources/calculations.py
+++ code/resources/calculations.py
@@ -40,7 +40,7 @@ def get_struts(thicksCSV):
 
 
 def get_ductileData(CSVout, crit=0.25, delimiter=','):
-    output_df = pd.read_csv(CSVout, names=['x', 'y'], usecols=['x', 'y'])#, delimiter=delimiter)
+    output_df = pd.read_csv(CSVout, names=['x', 'y'], usecols=['x', 'y'], delimiter=delimiter)
     e = [0] + output_df.x.tolist()[1:]
     s = [0] + output_df.y.tolist()[1:]
     s_sm = smooth(smooth(s))
