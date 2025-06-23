d4b1f9aae6f442e7616e61ef93c48ede901919b1
Niccolo Forte <exy053@qmul.ac.uk>
2025-06-23 16:02:40 +0100

    Fix.

--- code/resources/MLfunc.py
+++ code/resources/MLfunc.py
@@ -187,9 +187,9 @@ def train_model(typ, model, lossf, n_epochs, opt, train_dataloader, val_dataload
 
         if verbose:
             if epoch == 1 or epoch % int(verbose) == 0:
-                print("Epoch:", epoch, "- Loss:", lossAvg)
+                print("Epoch:", epoch, "- Loss:", lossAvg.item())
             
-    print(f"Best Epoch: {best_epoch}, with loss {best_loss.item()}")
+    print(f"Best Epoch: {best_epoch}, with loss {best_loss}")
     return model, epoch, train_lossLog, val_lossLog, best_loss
 
 def predict_model(typ, model, test_dataloader):
