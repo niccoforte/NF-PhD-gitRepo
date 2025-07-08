9da31e49933daaab33f6f264225b8699bec88bb2
Niccolo Forte <exy053@qmul.ac.uk>
2025-07-08 18:49:32 +0100

    Triangular xs run.

--- SIMscripts/run-local.ps1
+++ SIMscripts/run-local.ps1
@@ -1,6 +1,6 @@
-$LAT="FCC"
+$LAT="tri"
 $DIS="disNodes"
-$nnx=16
+$nnx=30
 $unitCellSize=10
 $mode="ductile"
 $material="ti"
@@ -10,15 +10,16 @@ $nJobs=100
 $CPUs=12
 $Fout=100
 $Hout=200
-$dir="Z:\\p1\sims\\Ti\\dimReductionData"
+$dir="Z:\\p1\sims\\Ti\\TargetedDisorder\\xs\\tri"
 
 Set-Location C:\temp
 
-for ($i = 1; $i -le 901; $i += 100) {
+for ($i = 1; $i -le 101; $i += 100) {
 	$initial=$i
 	abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A1_FractureToughness-Ductility.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
 }
 
-# abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_OUTpostProcess.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
 abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_INpostProcess.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
+abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_OUTpostProcess.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
+
 Set-Location C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\
