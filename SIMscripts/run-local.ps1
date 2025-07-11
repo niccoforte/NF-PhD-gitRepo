b8ff62bc9a96a39605748c5a23f77fe57e98e5f5
Niccolo Forte <exy053@qmul.ac.uk>
2025-07-11 12:17:21 +0100

    Cleanup. sys.argv input fix. Path spec cleanup.

--- SIMscripts/run-local.ps1
+++ SIMscripts/run-local.ps1
@@ -1,25 +1,28 @@
 $LAT="tri"
-$DIS="disNodes"
 $nnx=30
 $unitCellSize=10
 $mode="ductile"
 $material="ti"
 $rD=0.2
+$DIS="disNodes"
+$fac=0.2
+$distribution="frequency"
+$target="all"
 $initial=1
 $nJobs=100
 $CPUs=12
 $Fout=100
 $Hout=200
-$dir="Z:\\p1\sims\\Ti\\TargetedDisorder\\xs\\tri"
+$dir="Z:\\p1\sims\\Ti\\FrequencyDisorder\\FCC"
 
 Set-Location C:\temp
 
-for ($i = 1; $i -le 101; $i += 100) {
+for ($i = 1; $i -le 401; $i += 100) {
 	$initial=$i
-	abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A1_FractureToughness-Ductility.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
+	abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A1_FractureToughness-Ductility.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $dir
 }
 
-abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_INpostProcess.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
-abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_OUTpostProcess.py -- $LAT $DIS $nnx $unitCellSize $mode $material $rD $initial $nJobs $CPUs $Fout $Hout $dir
+abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_INpostProcess.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $dir
+abaqus cae noGUI=C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\A2_OUTpostProcess.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $dir
 
 Set-Location C:\Users\exy053\Documents\p1git-Lattices\SIMscripts\
