84e5534876bce326e2f214eda0cb1303ebe7c368
Niccolo Forte <exy053@qmul.ac.uk>
2025-07-28 09:54:03 +0100

    Increase path robustness of transfer script.

--- SIMscripts/B3_transfer-unzip.sh
+++ SIMscripts/B3_transfer-unzip.sh
@@ -1,21 +1,36 @@
-echo LAT:
+echo "LAT:"
 read LAT
 
-# LAT=tri
+echo "Extra Path Spec (CHECK THIS)"
+read EXTRA
 
-cd Z:/p1/data/Ti/20disNodes/$LAT/
 
-# rm -rf transfer
+#============ Transfer files from remote to local server ============
 
-mkdir transfer
-cd transfer
+mkdir -p Z:/p1/data/Ti/disNodes/$EXTRA/0.2/$LAT/transfer
+mkdir -p Z:/p1/data/Ti/per/$LAT/transfer
+cd Z:/p1/data/Ti/disNodes/$EXTRA/0.2/$LAT/transfer
 
-scp exy053@login.hpc.qmul.ac.uk:/data/SEMS-TaoLab/Niccolo-Forte/Ti/data/*/*/$LAT/*/zip/transfer/* "`pwd`"
+scp exy053@login.hpc.qmul.ac.uk:/data/SEMS-TaoLab/Niccolo-Forte/Ti/data/disNodes/$EXTRA/0.2/$LAT/*/zip/transfer/* exy053@login.hpc.qmul.ac.uk:/data/SEMS-TaoLab/Niccolo-Forte/Ti/data/per/0.0/$LAT/*/zip/transfer/* "`pwd`"
 
-mkdir Z:/p1/data/Ti/per/$LAT/transfer
 cp *per* Z:/p1/data/Ti/per/$LAT/transfer
+
+
+# #============ Transfer files from local to remote server ============
+
+# LOCAL_DIR="Z:/p1/sims/Ti/FrequencyDisorder/$LAT"
+# REMOTE_DIR="/data/SEMS-TaoLab/Niccolo-Forte/Ti/data/disNodes/Frequency/0.2/$LAT/local/zip"
+
+# cd $LOCAL_DIR
+
+# ssh exy053@login.hpc.qmul.ac.uk "mkdir -p $REMOTE_DIR"
+# # scp -r *.inp *.odb transfer exy053@login.hpc.qmul.ac.uk:"$REMOTE_DIR"
+# scp -r transfer exy053@login.hpc.qmul.ac.uk:"$REMOTE_DIR"
+
+
+# #============ Unzip files ============
 
 # tar -xvzf C*_$loc-$LAT-$DIS-$JOBn.tgz
 # cp -r data/scratch/exy053/$JOBn/$loc/* "`pwd`"
 # rm -rf data
-# rm -rf C*_$loc-$LAT-$DIS-$JOBn.tgz
+# rm -rf C*_$loc-$LAT-$DIS-$JOBn.tgz 
