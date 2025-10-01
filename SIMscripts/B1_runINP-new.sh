54c7681ab748cf55c48cf87afd3a686de9d24f39
Niccolo Forte - Desktop <exy053@qmul.ac.uk>
2025-10-01 16:50:33 +0100

    Adapt to correct format.

--- SIMscripts/B1_runINP-new.sh
+++ SIMscripts/B1_runINP-new.sh
@@ -33,6 +33,9 @@ Hout=200
 pDir=None
 
 PATH_EXTRA=
+OG_JOB=123
+
+ORIGIN_DIR=/data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$OG_JOB/zip #$SGE_O_WORKDIR
 
 zip=false
 delete_scratch=true
@@ -54,9 +57,9 @@ mkdir /data/scratch/$USER/$JOB_ID/zip/transfer/
 
 
 # copy command
-rsync -av $SGE_O_WORKDIR/A* /data/scratch/$USER/$JOB_ID
-rsync -av $SGE_O_WORKDIR/B* /data/scratch/$USER/$JOB_ID
-rsync -av $SGE_O_WORKDIR/Fracture*.inp /data/scratch/$USER/$JOB_ID
+rsync -av $ORIGIN_DIR/A* /data/scratch/$USER/$JOB_ID
+rsync -av $ORIGIN_DIR/B* /data/scratch/$USER/$JOB_ID
+rsync -av $ORIGIN_DIR/Fracture*.inp /data/scratch/$USER/$JOB_ID
 cd /data/scratch/$USER/$JOB_ID
 
 /bin/echo Working in directory: `pwd`.
@@ -89,23 +92,23 @@ for inp_file in "${inp_files[@]}"; do
     /bin/echo "----------------------------------------------------"
 done
 
-rsync -av $SGE_O_WORKDIR/Fracture*.inp /data/scratch/$USER/$JOB_ID
+rsync -av $ORIGIN_DIR/Fracture*.inp /data/scratch/$USER/$JOB_ID
 
 /bin/echo Simulation completed at: `date`.
 /bin/echo Processing outputs...
 
 abaqus cae noGUI=A-HPC-2_OUTpostProcess.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $pDir
-abaqus cae noGUI=A-HPC-2_INpostProcess.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $pDir
+# abaqus cae noGUI=A-HPC-2_INpostProcess.py -- $LAT $nnx $unitCellSize $mode $material $rD $DIS $fac $distribution $target $initial $nJobs $CPUs $Fout $Hout $pDir
 
 /bin/echo Inputs and outputs collected.
 
 
 # file transfer
-rsync -av /data/scratch/$USER/$JOB_ID/A* /data/scratch/$USER/$JOB_ID/zip/
-rsync -av /data/scratch/$USER/$JOB_ID/B* /data/scratch/$USER/$JOB_ID/zip/
-rsync -av /data/scratch/$USER/$JOB_ID/abaqus* /data/scratch/$USER/$JOB_ID/zip/
+# rsync -av /data/scratch/$USER/$JOB_ID/A* /data/scratch/$USER/$JOB_ID/zip/
+# rsync -av /data/scratch/$USER/$JOB_ID/B* /data/scratch/$USER/$JOB_ID/zip/
+# rsync -av /data/scratch/$USER/$JOB_ID/abaqus* /data/scratch/$USER/$JOB_ID/zip/
 rsync -av /data/scratch/$USER/$JOB_ID/*.odb /data/scratch/$USER/$JOB_ID/zip/
-rsync -av /data/scratch/$USER/$JOB_ID/*.inp /data/scratch/$USER/$JOB_ID/zip/
+# rsync -av /data/scratch/$USER/$JOB_ID/*.inp /data/scratch/$USER/$JOB_ID/zip/
 rsync -av /data/scratch/$USER/$JOB_ID/transfer/* /data/scratch/$USER/$JOB_ID/zip/transfer/
 
 /bin/echo Simulation files in /data/scratch/$USER/$JOB_ID/zip.
@@ -115,20 +118,13 @@ rsync -av /data/scratch/$USER/$JOB_ID/transfer/* /data/scratch/$USER/$JOB_ID/zip
 rsync -av $SGE_O_WORKDIR/$JOB_NAME.o$JOB_ID /data/scratch/$USER/$JOB_ID/
 rsync -av $SGE_O_WORKDIR/$JOB_NAME.o$JOB_ID /data/scratch/$USER/$JOB_ID/zip/
 
-mkdir /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$JOB_ID/
-mkdir /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$JOB_ID/zip/
-rsync -av /data/scratch/$USER/$JOB_ID/zip/* /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$JOB_ID/zip/
-
-if [ "$zip" = true ] ; then
-    tar -czf C1_transfer-$LAT-$DIS-$JOB_ID.tgz /data/scratch/$USER/$JOB_ID/transfer/
-    tar -czf C2_zip-$LAT-$DIS-$JOB_ID.tgz /data/scratch/$USER/$JOB_ID/zip/
-    rsync -av /data/scratch/$USER/$JOB_ID/C1_transfer-$LAT-$DIS-$JOB_ID.tgz $SGE_O_WORKDIR
-    rsync -av /data/scratch/$USER/$JOB_ID/C2_zip-$LAT-$DIS-$JOB_ID.tgz /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/
-fi
+# mkdir /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$JOB_ID/
+# mkdir /data/SEMS-TaoLab/Niccolo-Forte/Ti/data/$DIS/$PATH_EXTRA/$fac/$LAT/$JOB_ID/zip/
+rsync -av /data/scratch/$USER/$JOB_ID/zip/* $ORIGIN_DIR
 
 if [ "$delete_scratch" = true ] ; then
     rm -rf /data/scratch/$USER/$JOB_ID
 fi
 
 /bin/echo Job completed at: `date`.
-/bin/echo Finished. Data saved in $SGE_O_WORKDIR and /data/SEMS-TaoLab/Niccolo-Forte/
+/bin/echo Finished. Data saved in $ORIGIN_DIR and /data/SEMS-TaoLab/Niccolo-Forte/
