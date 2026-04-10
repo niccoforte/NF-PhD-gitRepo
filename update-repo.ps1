1156de3101d7d259f9b0e5eb05e3d00acb13a53b
Niccolo Forte - Desktop <exy053@qmul.ac.uk>
2026-04-10 10:05:03 +0100

    Update repo name in local instances for auto-commit loop.

--- update-repo.ps1
+++ update-repo.ps1
@@ -1,21 +1,21 @@
-cd C:\Users\exy053\Documents\p1git-Lattices
+cd "C:\Users\exy053\Documents\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(Local) Auto-commit."
 git push
 
-cd "C:\Users\exy053\OneDrive - Queen Mary, University of London\Documents\Research\p1-LatticeFractureToughness\p1git-Lattices"
+cd "C:\Users\exy053\OneDrive - Queen Mary, University of London\Documents\Research\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(OneDrive) Auto-commit."
 git push
 
-cd Z:\p1\p1git-Lattices
+cd "Z:\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(Z: Drive) Auto-commit."
 git push
 
-cd C:\Users\exy053\Documents\p1git-Lattices
+cd "C:\Users\exy053\Documents\00-PhD-gitRepo"
 git pull
 ssh -i C:\Users\exy053\.ssh\id_rsa exy053@login.hpc.qmul.ac.uk
