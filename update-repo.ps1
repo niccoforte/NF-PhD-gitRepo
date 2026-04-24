008ce67a702f0a8b5509f72ca5655e6adc02afb6
Niccolo Forte - Desktop <exy053@qmul.ac.uk>
2026-04-24 12:19:04 +0100

    Add public remote instance on github.com

--- update-repo.ps1
+++ update-repo.ps1
@@ -2,19 +2,20 @@ cd "C:\Users\exy053\Documents\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(Local) Auto-commit."
-git push
+git push origin main
 
 cd "C:\Users\exy053\OneDrive - Queen Mary, University of London\Documents\Research\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(OneDrive) Auto-commit."
-git push
+git push origin main
+git push public main
 
 cd "Z:\00-PhD-gitRepo"
 git pull
 git add .
 git commit -m "(Z: Drive) Auto-commit."
-git push
+git push origin main
 
 cd "C:\Users\exy053\Documents\00-PhD-gitRepo"
 git pull
